7d61c46f77d9d24b23a088a4ac8318f424acb618
Angel Ramirez <pepe_199904@hotmail.com>
2021-07-23 21:08:30 -0500

    Add Styled-Components, socialicons, homev1.0

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import Logo from './images/logo.png'
 
 function App() {
   return (
-    <div>
-      <h1>Hola</h1>
+    <main className="w-full flex inset-0 font-sans text-gray-900 bg-themeDarkBlue">
+    <div
+      className="w-full inset-0 
+                  h-screen text-white flex 
+                  justify-center items-center font-extrabold 
+                  text-xl"
+    >
+      <div className="flex-1 container mx-16">
+        <p
+          className="font-extrabold text-8xl sm:text-9xl"
+        >
+          ANGEL
+        </p>
+        <p
+          className="text-themeBlue font-extrabold text-8xl sm:text-9xl text-right"
+        >
+          RAMIREZ
+        </p>
+      </div>
+      <div className="flex-1">
+        <img className="hidden sm:block" src={Logo} />
+      </div>
     </div>
+  </main>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        themeBlue: '#6196E4',
+        themeDarkBlue: '#020215',
+      },
+    },
   },
   variants: {
     extend: {},
