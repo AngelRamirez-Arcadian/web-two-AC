6c1e47992db3932582c01561ad889a02e5e1b4a9
Angel Ramirez <pepe_199904@hotmail.com>
2021-08-05 01:16:57 -0500

    ProgressBar Approved

--- src/testprogress.js
+++ src/testprogress.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Prueba = () =>
 <section className="bg-themeDarkBlue pb-40"> 
     <h1 className="text-themeBlue font-semibold sm:font-bold text-3xl flex justify-center mb-10">Habilidades</h1>
-    <div className="sm:flex sm:flex-row grid grid-cols-2">
+    <div className="sm:flex sm:flex-row grid grid-cols-2 sm:mb-28 mb-20">
         <div className="flex-1 flex justify-center flex-col items-center mb-5 sm:mb-0">
             <div className="progress--circle progress--90">
                 <div className="progress__number font-IBM">90%</div>
@@ -27,6 +27,46 @@ const Prueba = () =>
                 <div className="progress__number font-IBM">70%</div>
             </div>
             <h1 className="text-themeBlue font-bold tracking-wider">Trabajo en equipo</h1>
+        </div>
+    </div>
+    <div className="w-full flex sm:flex-row flex-col">
+        <div className="sm:w-1/2 w-full container mx-auto sm:px-32 px-10">
+            <div className="relative pt-1">
+                <div className="flex mb-2 items-center justify-between">
+                    <div>
+                        <span className="text-xs font-semibold inline-block uppercase rounded-full text-themeBlue bg-themeDarkBlue">
+                            Task in progress
+                        </span>
+                    </div>
+                    <div className="text-right">
+                        <span className="text-xs font-semibold inline-block text-themeBlue">
+                            30%
+                        </span>
+                    </div>
+                </div>
+                <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-white">
+                    <div style={{ width: "30%" }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-themeBlue"></div>
+                </div>
+            </div>
+        </div>
+        <div className="sm:w-1/2 w-full container mx-auto sm:px-32 px-10">
+            <div className="relative pt-1">
+                <div className="flex mb-2 items-center justify-between">
+                    <div>
+                        <span className="text-xs font-semibold inline-block uppercase rounded-full text-themeBlue bg-themeDarkBlue">
+                            Task in progress
+                        </span>
+                    </div>
+                    <div className="text-right">
+                        <span className="text-xs font-semibold inline-block text-themeBlue">
+                            30%
+                        </span>
+                    </div>
+                </div>
+                <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-white">
+                    <div style={{ width: "30%" }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-themeBlue"></div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
