79e8627ea851c60cfce98a7ed3de3f77a0abb8e9
Angel Ramirez <pepe_199904@hotmail.com>
2021-08-05 20:35:32 -0500

    Skills v1.0.1 add circleBar component, fix design

--- src/skillSection.js
+++ src/skillSection.js
@@ -1,40 +1,37 @@
 import React from 'react';
+import CircleBar from './circleBar';
 import BarComponent from './barcomponent';
 
 const SkillSection = () =>
 <section className="bg-themeDarkBlue pb-40">
     <div className="sm:flex sm:flex-row flex-col">
         <div className="sm:w-1/2 w-w-full">
-            <h1 className="text-themeBlue font-semibold sm:font-bold text-3xl flex justify-center mb-10">Habilidades</h1>
-            <div className=" grid grid-cols-2 sm:mb-28 mb-20">
-                <div className="flex-1 flex justify-center flex-col items-center mb-5 sm:mb-0">
-                    <div className="progress--circle progress--90">
-                        <div className="progress__number font-IBM">90%</div>
-                    </div>
-                    <h1 className="text-themeBlue font-IBM font-semibold tracking-wider">Innovador</h1>
-                </div>
-                <div className="flex-1 flex justify-center flex-col items-center mb-5 sm:mb-0">
-                    <div className="progress--circle progress--80">
-                        <div className="progress__number font-IBM">80%</div>
-                    </div>
-                    <h1 className="text-themeBlue font-IBM font-semibold tracking-wider">Comunicacion</h1>
-                </div>
-                <div className="flex-1 flex justify-center flex-col items-center">
-                    <div className="progress--circle progress--100">
-                        <div className="progress__number font-IBM">100%</div>
-                    </div>
-                    <h1 className="text-themeBlue font-IBM font-semibold tracking-wider">Compromiso</h1>
-                </div>
-                <div className="flex-1 flex justify-center flex-col items-center">
-                    <div className="progress--circle progress--70">
-                        <div className="progress__number font-IBM">70%</div>
-                    </div>
-                    <h1 className="text-themeBlue font-IBM font-semibold tracking-wider text-center">Trabajo en equipo</h1>
-                </div>
+            <h1 className="text-themeBlue font-semibold sm:font-bold text-3xl flex justify-center">Habilidades</h1>
+            <div className=" grid grid-cols-2">
+                <CircleBar 
+                    classNames={'progress--90'}
+                    percent={'90%'}
+                    task={'Innovador'}
+                />
+                <CircleBar 
+                    classNames={'progress--80'}
+                    percent={'80%'}
+                    task={'Comunicacion'}
+                />
+                <CircleBar 
+                    classNames={'progress--100'}
+                    percent={'100%'}
+                    task={'Compromiso'}
+                />
+                <CircleBar 
+                    classNames={'progress--75'}
+                    percent={'75%'}
+                    task={'Trabajo en equipo'}
+                />
             </div>
         </div>
-        <div className="sm:w-1/2 w-full sm:mb-0 mb-10">
-            <h1 className="text-themeBlue font-semibold sm:font-bold text-3xl flex justify-center mb-10 text-center">Habilidades No Tecnicas</h1>
+        <div className="sm:w-1/2 w-full sm:mb-0">
+            <h1 className="text-themeBlue font-semibold sm:font-bold text-3xl flex justify-center mb-5 mt-10 desktop:mt-0  text-center">Habilidades No Tecnicas</h1>
             <div className="flex justify-center">
                 <ul className="text-white font-IBM font-semibold text-md leading-loose tracking-wide">
                     <li><font className="text-themeBlue">&#8226; </font>Analisis de Requerimientos</li>
@@ -46,8 +43,8 @@ const SkillSection = () =>
             </div>
         </div>
     </div>
-    <div className="w-full flex sm:flex-row flex-col mx-auto">
-        <div className="sm:w-1/3 w-full container mx-auto sm:px-14 px-12">
+    <div className="w-full flex sm:flex-row flex-col mx-auto mt-10 desktop:px-20">
+        <div className="desktop:w-1/3 w-full container mx-auto desktop:px-14 px-12">
             <h1 className="text-white text-2xl font-medium font-sans">FRONT END</h1>
             <BarComponent
                 title={'ReactJS'}
@@ -65,8 +62,8 @@ const SkillSection = () =>
                 stylePercent={'80%'}
             />
         </div>
-        <div className="sm:w-1/3 w-full container mx-auto sm:px-14 px-12">
-        <h1 className="text-white text-2xl font-medium font-sans">BACK END</h1>
+        <div className="desktop:w-1/3 w-full container mx-auto desktop:px-14 px-12">
+        <h1 className="text-white text-2xl font-medium font-sans mt-10 sm:mt-0">BACK END</h1>
             <BarComponent
                 title={'SQL SERVER'}
                 percent={'80%'}
@@ -88,8 +85,8 @@ const SkillSection = () =>
                 stylePercent={'25%'}
             />
         </div>
-        <div className="sm:w-1/3 w-full container mx-auto sm:px-14 px-12">
-            <h1 className="text-white text-2xl font-medium font-sans">MÁS</h1>
+        <div className="desktop:w-1/3 w-full container mx-auto desktop:px-14 px-12">
+            <h1 className="text-white text-2xl font-medium font-sans mt-10 sm:mt-0">MÁS</h1>
             <BarComponent
                 title={'Git / Github'}
                 percent={'90%'}
